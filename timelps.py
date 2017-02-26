a26a56148a93c1e870359bf7e0d625ab6155b423
papfant <yohannc74@hotmail.fr>
2017-02-26 15:53:11 +0100

    repar error

--- timelps.py
+++ timelps.py
@@ -2,9 +2,10 @@ from picamera import PiCamera
 from time import sleep
 from time import time
 from time import localtime
+from time import strftime
 
 def shutdownpi():
-    command = "/usr/bin/sudo /sbin/shutdown -h 1""
+    command = "/usr/bin/sudo /sbin/shutdown -h 1"
     import subprocess
     process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
     output = process.communicate()[0]
@@ -15,13 +16,13 @@ if localtime()[3] == 12:
 		camera.resolution = (2592,1944)
 		camera.framerate = 15
 		sleep(5)
-		camera.capture('/home/pi/TimeLapseHerve/image%.jpg' % time.strftime('%Y-%m-%d_%H%M%S'))
+		camera.capture('/home/pi/TimeLapseHerve/image%.jpg' % strftime('%Y-%m-%d_%H%M%S'))
 else:
 	with PiCamera() as camera:
 		camera.resolution = (2592,1944)
 		camera.framerate = 15
 		sleep(5)
-		camera.capture('/home/pi/TimeLapseHerve/image%.jpg' % time.strftime('%Y-%m-%d_%H%M%S'))
+		camera.capture('/home/pi/TimeLapseHerve/image%.jpg' % strftime('%Y-%m-%d_%H%M%S'))
 	#sleep(294)	
 sleep(2)
 shutdownpi()
