87106e8319ae7f6095a1f40852a119d2c76957ec
papfant <yohannc74@hotmail.fr>
2017-02-26 17:08:51 +0100

    add option for shutdown

--- timelps.py
+++ timelps.py
@@ -3,6 +3,12 @@ from time import sleep
 from time import time
 from time import localtime
 from time import strftime
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-a", "--shut", dest = "shut", help = "if a = 1, shutdown a fter picture")
+(options, args) = parser.parse_args()
+
 
 def shutdownpi():
     command = "/usr/bin/sudo /sbin/shutdown -h 1"
@@ -16,13 +22,18 @@ if localtime()[3] == 12:
 		camera.resolution = (2592,1944)
 		camera.framerate = 15
 		sleep(5)
+		print('Ready to capture')
 		camera.capture('/home/pi/TimeLapseHerve/image' + strftime('%Y-%m-%d_%H%M%S') + '.jpg')
 else:
+	print('Wait ... Thats not the good time now ...')
 	with PiCamera() as camera:
 		camera.resolution = (2592,1944)
 		camera.framerate = 15
 		sleep(5)
+		print('Ready to capture')
 		camera.capture('/home/pi/TimeLapseHerve/image' + strftime('%Y-%m-%d_%H%M%S') + '.jpg')
 	#sleep(294)	
-sleep(180)
-shutdownpi()
+if options.shut:
+	sleep(60)
+	shutdownpi()
+	print('Ive send the signal to nap')
